2ffc44836394184916dbfd53ca0c1ea171263dc8
RickelviC <Cruzrickelvi@gmail.com>
2026-05-12 12:09:28 -0400

    added logic to the override of getValue in House.java

--- src/main/java/com/pluralsight/House.java
+++ src/main/java/com/pluralsight/House.java
@@ -1,4 +1,71 @@
 package com.pluralsight;
 
-public class House {
+public class House extends Asset {
+
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotSize;
+
+    public House(String description, String dateAcquired,
+                 double originalCost, String address,
+                 int condition, int squareFoot, int lotSize) {
+        super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+
+
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+
+    @Override
+    public double getValue() {
+        double value = 0;
+        if (condition == 1){
+            value = 180.00 * squareFoot;
+        } else if (condition == 2) {
+            value = 130.00 * squareFoot;
+        }else if (condition == 3) {
+            value = 90.00 * squareFoot;
+        }else if (condition == 4) {
+            value = 80.00 * squareFoot;
+        }
+
+        value += .25 *squareFoot;
+
+        return value;
+    }
 }
